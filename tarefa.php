b2a129d72bdd27ad72bc79a66809059b357d5647
luiza <134729599+mlads15@users.noreply.github.com>
2024-02-23 22:17:50 -0300

    Update tarefa.php

--- tarefa.php
+++ tarefa.php
@@ -1,8 +1,12 @@
 <?php
 
-$numero1;
-$numero2;
+$multiplica = 0;
 
 $numero1 = readline("Escolha um número para essa variável: ");
 $numero2 = readline ("\nAgora, escolha outro número para a segunda variável: ");
+
+for($i = 1; $i <= $numero2; $i++){
+  $multiplica = $multiplica + $numero1;
+}
+echo "O resultado da multiplicação é: " . $multiplica . "\n";
 
